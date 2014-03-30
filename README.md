bfba3fcf4315a8a0c577baa327192e4b37f6c62a
Imran A <imran@virtual.co.uk>
2014-03-30 16:37:12 +0100

    added ftp deploy

--- README.md
+++ README.md
@@ -1,10 +1,11 @@
 boilerplate-html
 =================
 
-Basic setup for web app with static server and html5 boilerplate
+Basic setup for web app with static server and html5 boilerplate. Added ftp deployment
 
 - html5-boilerplate-4.3.0
 - grunt-contrib-connect-0.7.1
 - grunt-contrib-watch-0.6.1
 - grunt-contrib-less-0.11.0
+- grunt-ftp-deploy-0.1.2
 
--- gruntfile.js
+++ gruntfile.js
@@ -45,6 +45,19 @@ module.exports = function (grunt) {
                     "<%= pkg.name %>/css/site.css": "<%= pkg.name %>/css/site.less"
                 }
             }
+        },
+        
+        // ftp access
+        'ftp-deploy': {
+            build: {
+                auth: {
+                    host: 'server.com',
+                    port: 21,
+                    authKey: 'server.com'
+                },
+                src: '<%= pkg.name %>',
+                dest: ''
+            }
         }
         
     });
@@ -52,7 +65,9 @@ module.exports = function (grunt) {
     grunt.loadNpmTasks('grunt-contrib-watch');
     grunt.loadNpmTasks('grunt-contrib-connect');
     grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-ftp-deploy');
 
+    grunt.registerTask('push', ['ftp-deploy:build']);
     grunt.registerTask('default', ['connect:server', 'watch']);
 
 };
